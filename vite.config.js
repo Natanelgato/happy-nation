ca63382a9ec1f3c441e3f1edfd6d6fa63e1523dc
Natanelgato <natanfelipepessoal@gmail.com>
2025-05-30 03:26:39 -0300

    Primeiro deploy do site Happy Nation

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  root: '.', // garante que olhe na raiz
-  publicDir: 'public',
-  build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'public/index.html')
-    },
-    outDir: 'dist'
-  }
-});
+  plugins: [react()]
+})

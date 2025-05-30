7b498aa6acad23a8c6055564df0e81ebfb859790
Natanelgato <natanfelipepessoal@gmail.com>
2025-05-30 04:33:10 -0300

    Ajusta visual conforme mockup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import avatar from '../assets/avatar.png'
-import background from '../assets/background.jpg'
-
-const headerStyle = {
-  backgroundImage: `url(${background})`,
-  backgroundSize: 'cover',
-  padding: '2rem',
-  width: '100%',
-  color: '#fff',
-  textAlign: 'center'
-}
 
 function Header() {
   return (
-    <header style={headerStyle}>
-      <img src={avatar} alt="Avatar" style={{ width: '120px', borderRadius: '50%' }} />
+    <div style={{ marginTop: '1rem' }}>
+      <img src={avatar} alt="Avatar" className="header-avatar" />
       <h1>👑 Projeto Happy Nation 👑</h1>
       <p>Para a rainha mais linda, com amor e risadas! 💖</p>
-    </header>
+    </div>
   )
 }
 

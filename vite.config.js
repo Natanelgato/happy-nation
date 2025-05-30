9398d7998b7de6be39c94d29816890982af03de9
Natanelgato <natanfelipepessoal@gmail.com>
2025-05-30 03:15:17 -0300

    Corrige entrada da build no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  root: '.', // garante que olhe na raiz
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'public/index.html')
+    },
+    outDir: 'dist'
+  }
+});
